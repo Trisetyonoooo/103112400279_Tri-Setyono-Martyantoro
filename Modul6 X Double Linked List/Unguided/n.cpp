9be497294027285759e743dfb0e399530b9514c4
Tri Setyono <setyonotri8@gmail.com>
2025-10-27 04:16:27 +0700

    Add files via upload

---
+++ Modul6 X Double Linked List/Unguided/n.cpp
@@ -0,0 +1,7 @@
+
+procedure deleteFirst( input/output L : List,  
+P : address ) - - 
+procedure deleteLast( input/output L : List,  
+P : address ) 
+procedure deleteAfter( input Prec : address,  
+input/output P : address )
